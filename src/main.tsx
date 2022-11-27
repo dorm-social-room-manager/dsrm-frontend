17c36a5330504c4143a09c5b9244d5aad7ff13fd
macszl <235989@edu.p.lodz.pl>
2022-11-27 12:57:18 +0100

    chore: changed the import types in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import './index.scss';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
