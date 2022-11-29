cc6e46cb3042745ee121a3cb59ac6ea3a38c02be
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2022-11-29 20:14:55 +0100

    chore(workflow): add git-hooks to automatize work
    Related work item: GH-8

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import './index.scss';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
-
+const x = 12;
+console.log(x);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
