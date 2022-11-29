de2c195585077975dadd3e39fa3fb79ed4131636
macszl <235989@edu.p.lodz.pl>
2022-11-29 13:18:22 +0100

    refactor: npm start page now blank

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,5 @@
 import './App.scss';
-import reactLogo from './assets/react.svg';
-import { useState } from 'react';
 
 export function App() {
-  const initialState = 0;
-  const [count, setCount] = useState(initialState);
-
-  return (
-    <div className='App'>
-      <div>
-        <a
-          href='https://vitejs.dev'
-          target='_blank'
-        >
-          <img
-            src='/vite.svg'
-            className='logo'
-            alt='Vite logo'
-          />
-        </a>
-        <a
-          href='https://reactjs.org'
-          target='_blank'
-        >
-          <img
-            src={reactLogo}
-            className='logo react'
-            alt='React logo'
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button
-          onClick={() => {
-            return setCount((cnt) => {
-              const minIncrease = 1;
-              return cnt + minIncrease;
-            });
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+  return <div className='App'></div>;
 }
