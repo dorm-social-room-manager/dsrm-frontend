d2aa5076143b51e9517e16c4775f382db5b22bcf
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2022-12-01 01:49:31 +0100

    refactor(code-quality): remove unnecessary varialbe
    Related work item: GH-8

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import './index.scss';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
-const x = 12;
-console.log(x);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
