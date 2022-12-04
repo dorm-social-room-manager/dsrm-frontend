99ff30205006bdcdc0a857d05d070dc28cc5deb1
macszl <235989@edu.p.lodz.pl>
2022-12-04 16:49:22 +0100

    feat: draft version of a header component
    Related work item: GH-4

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import '@fontsource/roboto';
+import '@fontsource/roboto-mono';
+import { Grid, Link, Typography } from '@mui/material';
+
+export function Header() {
+  return (
+    <header>
+      <Grid
+        container
+        direction='column'
+        alignItems='center'
+        justifyContent='center'
+        spacing={0}
+        padding={0}
+      >
+        <Typography sx={{ borderBottom: 1, fontFamily: 'Roboto Mono', fontSize: 64, fontWeight: '500', letterSpacing: -1.5 }}>
+          Dorm room social manager
+        </Typography>
+        <div>
+          <Link
+            href='https://samorzad.p.lodz.pl/osiedle-akademickie/iv-dom-studenta
+'
+          >
+            IV DS
+          </Link>
+          <Typography>|</Typography>
+          <Link href='https://p.lodz.pl/'>Politechnika Łódzka</Link>
+        </div>
+      </Grid>
+    </header>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
 
 export function App() {
   return (
     <Routes>
       <Route
         path='/'
+        element={<Header />}
+      ></Route>
+      <Route
+        path='/'
         element={<LoginPage />}
+      />
+      <Route
+        path='/'
+        element={<Footer />}
       />
     </Routes>
   );
