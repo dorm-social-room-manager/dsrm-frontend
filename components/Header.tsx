a7c0e2fb8a5386ea2abef81419217f73d83ec635
macszl <235989@edu.p.lodz.pl>
2022-12-04 17:19:20 +0100

    fix: minor fixes to the header component
    Related work item: GH-4

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,18 +13,28 @@ export function Header() {
         spacing={0}
         padding={0}
       >
-        <Typography sx={{ borderBottom: 1, fontFamily: 'Roboto Mono', fontSize: 64, fontWeight: '500', letterSpacing: -1.5 }}>
+        <Typography
+          sx={{ borderBottom: 1, borderColor: '#0000001F', fontFamily: 'Roboto Mono', fontSize: 64, fontWeight: '500', letterSpacing: -1.5 }}
+        >
           Dorm room social manager
         </Typography>
-        <div>
+        <div style={{ alignItems: 'center', display: 'flex', fontFamily: 'Roboto Mono', fontSize: 16, gap: 16, letterSpacing: 0.15 }}>
           <Link
             href='https://samorzad.p.lodz.pl/osiedle-akademickie/iv-dom-studenta
 '
+            color='#1565C0'
+            underline='none'
           >
             IV DS
           </Link>
           <Typography>|</Typography>
-          <Link href='https://p.lodz.pl/'>Politechnika Łódzka</Link>
+          <Link
+            href='https://p.lodz.pl/'
+            color='#1565C0'
+            underline='none'
+          >
+            Politechnika Łódzka
+          </Link>
         </div>
       </Grid>
     </header>
