21a6ff172dfa2b409bbf11770b744759827a8867
macszl <235989@edu.p.lodz.pl>
2022-12-12 18:26:33 +0100

    fix: moved main.tsx back to src
    Related work item: GH-6

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import './main.scss';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { Footer } from '../../templates/Footer/Footer';
+import { Footer } from './templates/Footer/Footer';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
