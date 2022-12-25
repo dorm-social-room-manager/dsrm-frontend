0693864293f71fbf13472502ddec8afb66c653bd
macszl <235989@edu.p.lodz.pl>
2022-12-25 16:01:51 +0100

    fix: aligned the text to the left
    Related work item: GH-4

--- src/templates/Header/Header.tsx
+++ src/templates/Header/Header.tsx
@@ -6,47 +6,42 @@ import { useTheme } from '@mui/material/styles';
 export function Header(props: HeaderProps) {
   const theme = useTheme();
   return (
-    <header>
-      <Grid
-        container
-        direction='column'
-        alignItems='center'
-        justifyContent='center'
-        spacing={0}
-        padding={0}
+    <div className={styles.container}>
+      <Typography
+        sx={{
+          borderBottom: 1,
+          borderColor: theme.palette.action.disabledBackground,
+          fontFamily: 'Roboto Mono',
+          fontSize: theme.typography.h2.fontSize,
+          fontWeight: (them) => {
+            return them.typography.fontWeightMedium;
+          },
+          letterSpacing: -1.5,
+          maxWidth: 560,
+        }}
       >
-        <Typography
-          sx={{
-            borderBottom: 1,
-            borderColor: theme.palette.action.disabledBackground,
-            fontFamily: 'Roboto Mono',
-            fontSize: theme.typography.h2.fontSize,
-            fontWeight: (them) => {
-              return them.typography.fontWeightMedium;
-            },
-            letterSpacing: -1.5,
-          }}
+        {props.headerTitle}
+      </Typography>
+      <Grid
+        item
+        className={styles.links}
+      >
+        <Link
+          href={props.dormitory.url}
+          color={theme.palette.primary.dark}
+          underline='none'
         >
-          {props.headerTitle}
-        </Typography>
-        <div className={styles.links}>
-          <Link
-            href={props.dormitory.url}
-            color={theme.palette.primary.dark}
-            underline='none'
-          >
-            {props.dormitory.urlName}
-          </Link>
-          <Typography>|</Typography>
-          <Link
-            href={props.faculty.url}
-            color={theme.palette.primary.dark}
-            underline='none'
-          >
-            {props.faculty.urlName}
-          </Link>
-        </div>
+          {props.dormitory.urlName}
+        </Link>
+        <Typography>|</Typography>
+        <Link
+          href={props.faculty.url}
+          color={theme.palette.primary.dark}
+          underline='none'
+        >
+          {props.faculty.urlName}
+        </Link>
       </Grid>
-    </header>
+    </div>
   );
 }
