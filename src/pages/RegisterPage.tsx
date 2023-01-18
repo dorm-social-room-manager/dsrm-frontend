7863d32bda18c39a5dd918d807d3ae0de8232f47
Maciej Szlendak <72614768+macszl@users.noreply.github.com>
2023-01-19 00:54:31 +0100

    feat(register-page): add register page
    * feat: placed all necessary components into to the register page
    
    Related work item: GH-18
    
    * refactor: register page uses flex extensively now
    
    Related work item: GH-18
    
    * fix: added padding and fixed the footer component
    
    Related work item: GH-18

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,85 @@
+import { Box, Grid } from '@mui/material';
+import { Footer } from '../templates/Footer/Footer';
+import { Header } from '../templates/Header/Header';
+import { HeaderProps } from '../templates/Header/Header.types';
 import { RegisterForm } from '../components/RegisterForm/RegisterForm';
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
 
 export function RegisterPage() {
-  return <RegisterForm />;
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const tablet = useMediaQuery(theme.breakpoints.up('tablet'));
+  const header = tablet ? t(`registerForm.title`) : t(`registerForm.shortTitle`);
+
+  const paddingTopValueForRegisterFormWhenMobile = 10;
+  const paddingTopValueForRegisterFormWhenTablet = 0;
+  const paddingTopValueForRegisterForm = tablet ? paddingTopValueForRegisterFormWhenTablet : paddingTopValueForRegisterFormWhenMobile;
+
+  const paddingLeftRightValueForContainer = 2;
+  const paddingTopValueForFooterHeader = 2;
+
+  const headerProps: HeaderProps = {
+    dormitory: { url: 'https://samorzad.p.lodz.pl/osiedle-akademickie/iv-dom-studenta', urlName: t('header.buildingName') },
+    faculty: { url: 'https://www.p.lodz.pl/', urlName: t('header.faculty') },
+    headerTitle: header,
+  };
+  return (
+    <Grid
+      container
+      justifyContent={'center'}
+      alignItems={'flex-end'}
+      height={'100vh'}
+    >
+      <Grid
+        item
+        container
+        justifyContent={'center'}
+        alignItems={'center'}
+        paddingLeft={paddingLeftRightValueForContainer}
+        paddingRight={paddingLeftRightValueForContainer}
+        paddingTop={paddingTopValueForFooterHeader}
+      >
+        <Grid
+          item
+          mobile={12}
+          tablet={6}
+          desktop={6}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+            <Header {...headerProps}></Header>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          mobile={12}
+          tablet={6}
+          desktop={6}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <Box
+            paddingTop={paddingTopValueForRegisterForm}
+            sx={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}
+          >
+            <RegisterForm />
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        mobile={12}
+        tablet={12}
+        desktop={12}
+        alignSelf={'flex-end'}
+        paddingTop={paddingTopValueForFooterHeader}
+      >
+        <Footer></Footer>
+      </Grid>
+    </Grid>
+  );
 }
