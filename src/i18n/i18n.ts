d662d1a8aa3f0e2eada065a1e628c17cdd384022
235923 <78360174+235923@users.noreply.github.com>
2023-01-30 00:54:05 +0100

    feat: created dashboardPage (#31)
    * feat: created dashboardPage
    
    Related work item: GH-19
    
    * fix: typo
    
    Related work item: GH-19

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,4 +1,6 @@
 import * as dashboardEN from '../locales/en/dashboard.json';
+import * as dashboardPageEN from '../locales/en/dashboardPage.json';
+import * as dashboardPagePL from '../locales/pl/dashboardPage.json';
 import * as dashboardPL from '../locales/pl/dashboard.json';
 import * as footerEN from '../locales/en/footer.json';
 import * as footerPL from '../locales/pl/footer.json';
@@ -8,6 +10,7 @@ import * as loginFormEN from '../locales/en/loginForm.json';
 import * as loginFormPL from '../locales/pl/loginForm.json';
 import * as registerFormEN from '../locales/en/registerForm.json';
 import * as registerFormPL from '../locales/pl/registerForm.json';
+
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
@@ -15,6 +18,7 @@ const resources = {
   en: {
     components: {
       dashboard: dashboardEN,
+      dashboardPage: dashboardPageEN,
       footer: footerEN,
       header: headerEN,
       loginForm: loginFormEN,
@@ -24,6 +28,7 @@ const resources = {
   pl: {
     components: {
       dashboard: dashboardPL,
+      dashboardPage: dashboardPagePL,
       footer: footerPL,
       header: headerPL,
       loginForm: loginFormPL,
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,50 +4,67 @@ import { DashboardHeader } from '../components/DashboardHeader/DashboardHeader';
 import { DashboardHeaderProps } from '../components/DashboardHeader/DashboardHeader.types';
 import { DashboardItem } from '../components/DashboardCard/DashboardCard.types';
 import FeaturedPlayListSharpIcon from '@mui/icons-material/FeaturedPlayListSharp';
+import { Footer } from '../templates/Footer/Footer';
 import { Grid } from '@mui/material';
 import InsertPhotoSharpIcon from '@mui/icons-material/InsertPhotoSharp';
 import PersonIcon from '@mui/icons-material/Person';
+import { useTranslation } from 'react-i18next';
 
-const ADMIN_ITEMS: DashboardItem[] = [
-  {
-    icon: PersonIcon,
-    title: 'Users',
-    url: '/dashboard/users',
-  },
-  {
-    icon: InsertPhotoSharpIcon,
-    title: 'Rooms',
-    url: '/dashboard/rooms',
-  },
-  {
-    icon: FeaturedPlayListSharpIcon,
-    title: 'Reservations',
-    url: '/dashboard/reservations',
-  },
-  {
-    icon: AddPhotoAlternateSharpIcon,
-    title: 'Add room',
-    url: '/dashboard/add-room',
-  },
-];
 const dashboardHeaderProps: DashboardHeaderProps = {
   userAvatar: 'https://www.w3schools.com/howto/img_avatar.png',
   userName: 'John Doe',
 };
 export function DashboardPage() {
+  const { t } = useTranslation();
+  const ADMIN_ITEMS: DashboardItem[] = [
+    {
+      icon: PersonIcon,
+      title: t(`dashboardPage.users`),
+      url: '/dashboard/users',
+    },
+    {
+      icon: InsertPhotoSharpIcon,
+      title: t(`dashboardPage.rooms`),
+      url: '/dashboard/rooms',
+    },
+    {
+      icon: FeaturedPlayListSharpIcon,
+      title: t('dashboardPage.reservations'),
+      url: '/dashboard/reservations',
+    },
+    {
+      icon: AddPhotoAlternateSharpIcon,
+      title: t('dashboardPage.addRoom'),
+      url: '/dashboard/add-room',
+    },
+  ];
   return (
     <Grid
       container
       alignItems='center'
       justifyContent='center'
-      direction='column'
+      direction='row'
       height={'100vh'}
     >
-      <Grid item>
+      <Grid
+        mobile={12}
+        item
+      >
         <DashboardHeader {...dashboardHeaderProps} />
       </Grid>
-      <Grid item>
+      <Grid
+        item
+        mobile={12}
+        margin={19}
+      >
         <DashboardBody items={ADMIN_ITEMS} />
+      </Grid>
+      <Grid
+        item
+        mobile={12}
+        alignSelf={'flex-end'}
+      >
+        <Footer />
       </Grid>
     </Grid>
   );
