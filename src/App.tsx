e35b868e7ceab419e9cc90356ad3a445f0cd34d3
Wiktor Michałowski <235923@edu.p.lodz.pl>
2023-02-23 00:53:43 +0100

    feat: add room page draft
    Related work item: GH-26

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import { Route, Routes } from 'react-router-dom';
+import { AddRoomForm } from './components/AddRoomForm/AddRoomForm';
+import { AddRoomPage } from './pages/AddRoomPage';
 import { DashboardPage } from './pages/DashboardPage';
 import { LoginPage } from './pages/LoginPage';
 
 import { RegisterPage } from './pages/RegisterPage';
+import { RoomType } from './components/AddRoomForm/AddRoomForm.types';
 import { UserListPage } from './pages/UserListPage';
+
+const ROOM_TYPES: RoomType[] = [
+  {
+    id: 1,
+    name: 'STUDY',
+  },
+  {
+    id: 2,
+    name: 'TV',
+  },
+  {
+    id: 3,
+    name: 'BILLARD',
+  },
+  {
+    id: 4,
+    name: 'PING PONG',
+  },
+  {
+    id: 5,
+    name: 'LAUNDRY',
+  },
+];
 
 export function App() {
   return (
@@ -23,6 +49,14 @@ export function App() {
       <Route
         path='/dashboard/user-list'
         element={<UserListPage />}
+      />
+      <Route
+        path='/dashboard/add-room'
+        element={<AddRoomPage />}
+      />
+      <Route
+        path='/dashboard/add-room/t'
+        element={<AddRoomForm roomTypes={ROOM_TYPES} />}
       />
     </Routes>
   );
