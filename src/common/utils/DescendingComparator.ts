23a011f21dc10b5c40c28f4be61bd12f8d05b45d
macszl <235989@edu.p.lodz.pl>
2023-03-03 20:48:42 +0100

    fix: changed descendingComparator
    Related work item: GH-29

--- src/common/utils/DescendingComparator.ts
+++ src/common/utils/DescendingComparator.ts
@@ -1,8 +1,8 @@
-export function descendingComparator<T>(firstValue: T, secondValue: T, orderBy: keyof T) {
-  if (secondValue[orderBy] < firstValue[orderBy]) {
+export function descendingComparator<T>(recordA: T, recordB: T, fieldName: keyof T) {
+  if (recordB[fieldName] < recordA[fieldName]) {
     return -1;
   }
-  if (secondValue[orderBy] > firstValue[orderBy]) {
+  if (recordB[fieldName] > recordA[fieldName]) {
     return 1;
   }
   return 0;
