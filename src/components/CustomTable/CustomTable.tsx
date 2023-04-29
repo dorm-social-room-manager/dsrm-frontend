0fc627d60e2e53a288baba4ce60920d906badc07
macszl <235989@edu.p.lodz.pl>
2023-04-29 22:31:04 +0200

    fix: moved customtablehead to its own component
    Related work item: GH-29

--- src/components/CustomTable/CustomTable.tsx
+++ src/components/CustomTable/CustomTable.tsx
@@ -1,27 +1,12 @@
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel } from '@mui/material';
+import { Box, Table, TableBody, TableCell, TableContainer, TablePagination, TableRow } from '@mui/material';
 import { ChangeEvent, MouseEvent, useEffect } from 'react';
 import Checkbox from '@mui/material/Checkbox';
+import { CustomTableHead } from '../CustomTableHead/CustomTableHead';
 import { CustomTableProps } from './CustomTable.types';
 import { FetchError } from '../../errors/FetchError';
-import { SortingDirection } from '../../common/utils/SortingDirection';
-import { t } from 'i18next';
 
 export function CustomTable<T extends Record<PropertyKey, unknown>>(props: CustomTableProps<T>) {
-  const {
-    toolbar,
-    fetchUrl,
-    columnConfig,
-    selectedRowsIds,
-    page,
-    tableName,
-    rows,
-    rowsPerPage,
-    setColumnConfig,
-    setSelectedRowsIds,
-    setPage,
-    setRowsPerPage,
-    setRows,
-  } = props;
+  const { toolbar, fetchUrl, selectedRowsIds, page, rows, rowsPerPage, setSelectedRowsIds, setPage, setRowsPerPage, setRows } = props;
 
   const isSelected = (row: T) => {
     return (
@@ -30,12 +15,6 @@ export function CustomTable<T extends Record<PropertyKey, unknown>>(props: Custo
       }).length > 0
     );
   };
-
-  function updateSortingRule(index: number, columnName: string, sortDirection: SortingDirection) {
-    const updatedConfig = [...columnConfig];
-    updatedConfig.splice(index, 1, { id: columnName as keyof T, label: columnName.toUpperCase(), sortDirection: sortDirection });
-    setColumnConfig(updatedConfig);
-  }
 
   const pixelHeightPerRow = 53;
   const rowsPerPageArray = [5, 10, 25];
@@ -66,49 +45,6 @@ export function CustomTable<T extends Record<PropertyKey, unknown>>(props: Custo
   const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
-  };
-
-  const handleRequestSort = (_event: MouseEvent<unknown>, property: keyof T) => {
-    const idx = columnConfig.findIndex((config) => {
-      return config.id === property;
-    });
-
-    const isAsc = columnConfig[idx].id === property && columnConfig[idx].sortDirection === SortingDirection.ASC;
-    const newSortingDirection = isAsc ? SortingDirection.DESC : SortingDirection.ASC;
-
-    updateSortingRule(idx, String(property), newSortingDirection);
-
-    /* when backend synchronization will be done, this will send out a REST request*/
-  };
-
-  const handleSelectAllClick = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      //check whether the rows have been selected
-      const newSelectedRowsIds: readonly string[] = rows
-        .map((row, index) => {
-          return !isSelected(row) ? String(index) : String(-1);
-        })
-        .filter((index) => {
-          return index !== String(-1);
-        });
-
-      setSelectedRowsIds(selectedRowsIds.concat(newSelectedRowsIds));
-      return;
-    }
-
-    setSelectedRowsIds(
-      selectedRowsIds.filter((rowId) => {
-        return !rows.find((item) => {
-          return item.id === rowId;
-        });
-      })
-    );
-  };
-
-  const createSortHandler = (property: keyof T) => {
-    return (event: MouseEvent<unknown>) => {
-      handleRequestSort(event, property);
-    };
   };
 
   const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
@@ -167,52 +103,6 @@ export function CustomTable<T extends Record<PropertyKey, unknown>>(props: Custo
     );
   };
 
-  const renderHead = () => {
-    const label = t(`${tableName}.selectAll`);
-    return (
-      <TableHead>
-        <TableRow>
-          <TableCell padding='checkbox'>
-            <Checkbox
-              color='primary'
-              indeterminate={selectedRowsIds.length > 0 && selectedRowsIds.length < rows.length}
-              checked={rows.length > 0 && selectedRowsIds.length === rows.length}
-              onChange={handleSelectAllClick}
-              inputProps={{
-                'aria-label': label,
-              }}
-            />
-          </TableCell>
-          {columnConfig.map((configItem, index) => {
-            return (
-              <TableCell
-                key={String(configItem.id)}
-                align='left'
-                sortDirection={columnConfig[index].id === String(configItem.id) ? columnConfig[0].sortDirection : false}
-              >
-                <TableSortLabel
-                  active={columnConfig[index].id === String(configItem.id)}
-                  direction={columnConfig[index].id === String(configItem.id) ? columnConfig[0].sortDirection : SortingDirection.ASC}
-                  onClick={createSortHandler(configItem.id)}
-                >
-                  {configItem.label}
-                  {columnConfig[index].id === configItem.id ? (
-                    <Box
-                      component='span'
-                      sx={{ display: 'none' }}
-                    >
-                      {columnConfig[index].sortDirection === SortingDirection.DESC ? t(`${tableName}.sortDsc`) : t(`${tableName}.sortAsc`)}
-                    </Box>
-                  ) : null}
-                </TableSortLabel>
-              </TableCell>
-            );
-          })}
-        </TableRow>
-      </TableHead>
-    );
-  };
-
   return (
     <Box
       width='100%'
@@ -222,7 +112,7 @@ export function CustomTable<T extends Record<PropertyKey, unknown>>(props: Custo
 
       <TableContainer>
         <Table>
-          {renderHead()}
+          {<CustomTableHead {...props} />}
           <TableBody>
             {rows.map((row, index) => {
               return renderRow(row, index);
