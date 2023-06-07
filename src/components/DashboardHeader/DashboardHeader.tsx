6f3602de6f4ad912f88aba118147f1787e0f5217
LucasBywalec <68160884+LucasBywalec@users.noreply.github.com>
2023-06-07 22:58:36 +0200

    feat(user): add user profile button (#55)
    * Add user profile button
    
    Related work item: GH-46
    
    * simplified the avatar alt and src check
    
    Related work item: GH-46

--- src/components/DashboardHeader/DashboardHeader.tsx
+++ src/components/DashboardHeader/DashboardHeader.tsx
@@ -1,4 +1,20 @@
-import { AppBar, AppBarProps, Avatar, Box, Divider, Drawer, IconButton, List, styled, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  AppBarProps,
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { MouseEvent, useState } from 'react';
 import AddPhotoAlternateSharpIcon from '@mui/icons-material/AddPhotoAlternateSharp';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { DashboardHeaderProps } from './DashboardHeader.types';
@@ -9,8 +25,8 @@ import InsertPhotoSharpIcon from '@mui/icons-material/InsertPhotoSharp';
 import MenuIcon from '@mui/icons-material/Menu';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import PersonIcon from '@mui/icons-material/Person';
-import { t } from 'i18next';
-import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
 const DrawerHeader = styled('div')(({ theme }) => {
   return {
@@ -48,7 +64,43 @@ const MyAppBar = styled(AppBar, {
 
 const drawerWidth = 240;
 export function DashboardHeader(props: DashboardHeaderProps) {
+  const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
   const [open, setOpen] = useState(false);
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleOpenMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElement(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorElement(null);
+  };
+
+  const settings = [
+    {
+      action: () => {
+        navigate('/account');
+      },
+      key: 'account',
+      option: t('dashboardHeader.account'),
+    },
+    {
+      action: () => {
+        navigate('/dashboard');
+      },
+      key: 'dashboard',
+      option: t('dashboardHeader.dashboard'),
+    },
+    {
+      action: () => {
+        navigate('/');
+      },
+      key: 'logOut',
+      option: t('dashboardHeader.logOut'),
+    },
+  ];
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -121,12 +173,51 @@ export function DashboardHeader(props: DashboardHeaderProps) {
           >
             {props.logo}
           </Typography>
-          <IconButton>
-            <Avatar
-              alt={props.userName === undefined ? 'User' : props.userName}
-              src={props.userAvatar === undefined ? '' : props.userAvatar}
-            />
-          </IconButton>
+          <Box>
+            <Tooltip title='Open settings'>
+              <IconButton
+                onClick={handleOpenMenu}
+                sx={{ p: 0 }}
+              >
+                <Avatar
+                  alt={props.userName ?? 'User'}
+                  src={props.userAvatar ?? ''}
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id='menu-appbar'
+              anchorEl={anchorElement}
+              anchorOrigin={{
+                horizontal: 'right',
+                vertical: 'top',
+              }}
+              keepMounted
+              transformOrigin={{
+                horizontal: 'right',
+                vertical: 'top',
+              }}
+              open={Boolean(anchorElement)}
+              onClose={handleCloseMenu}
+            >
+              {settings.map((setting) => {
+                return (
+                  <MenuItem
+                    key={setting.key}
+                    onClick={handleCloseMenu}
+                  >
+                    <Typography
+                      textAlign='center'
+                      onClick={setting.action}
+                    >
+                      {setting.option}
+                    </Typography>
+                  </MenuItem>
+                );
+              })}
+            </Menu>
+          </Box>
         </Toolbar>
       </MyAppBar>
       <Drawer
